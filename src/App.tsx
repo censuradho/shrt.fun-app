8488720c5cecf590ed3fa8881a9ec7f4b569970a
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 15:34:51 -0300

    feat(app): wrap App component with QueryClientProvider for React Query integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 import { AuthProvider } from "./contexts/auth/auth.context";
 import { Toaster } from "sonner";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Toaster 
-          theme="dark" 
-          richColors 
-          position="bottom-right"
-          closeButton
-        />
-        <AppRoutes />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Toaster 
+            theme="dark" 
+            richColors 
+            position="bottom-right"
+            closeButton
+          />
+          <AppRoutes />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
