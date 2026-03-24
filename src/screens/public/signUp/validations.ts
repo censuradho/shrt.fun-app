ace087254d2ef175e3faf55a6947d207953ab0e8
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 15:34:13 -0300

    feat(signUp): enhance password validation with additional criteria

--- src/screens/public/signUp/validations.ts
+++ src/screens/public/signUp/validations.ts
@@ -1,9 +1,20 @@
 import { errorMessages } from "@/constants/errorMessages";
+import { hasLowercaseCharacter, hasNumber, hasSpecialCharacter, hasUppercaseCharacter } from "@/utils/passwordValidations";
 import z from "zod";
 
 export const signUpValidation = z.object({
   email: z.email('E-mail inválido'),
-  password: z.string().min(8, errorMessages.minLength(8)).max(256, errorMessages.maxLength(256)),
+  password: z
+    .string()
+    .min(8, '   ')
+    .max(256, errorMessages.maxLength(256))
+    .refine(value => 
+      hasLowercaseCharacter(value) &&
+      hasNumber(value) && 
+      hasUppercaseCharacter(value) &&
+      hasSpecialCharacter(value),
+    '   '
+    ),
   firstName: z.string().min(1, errorMessages.required).max(100, errorMessages.maxLength(100)),
   lastName: z.string().min(1, errorMessages.required).max(255, errorMessages.maxLength(255)),
   terms: z.literal(true, 'Você deve aceitar os termos e condições')
