53c638c64f8a5e1a45de2157b14e14d93ee72fb1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 11:52:18 -0300

    feat(App): add Toaster component for enhanced notification support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 import { AuthProvider } from "./contexts/auth/auth.context";
+import { Toaster } from "sonner";
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthProvider>
+        <Toaster 
+          theme="dark" 
+          richColors 
+          position="bottom-right"
+          closeButton
+        />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
