b619736c1646dcb07b039ad0dc035a59b4bf2460
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 13:29:33 -0300

    feat(validations): add signUpValidation schema for user registration

---
+++ src/screens/public/signUp/validations.ts
@@ -0,0 +1,11 @@
+import { errorMessages } from "@/constants/errorMessages";
+import z from "zod";
+
+export const signUpValidation = z.object({
+  email: z.email('E-mail inválido'),
+  password: z.string().min(8, errorMessages.minLength(8)).max(256, errorMessages.maxLength(256)),
+  firstName: z.string().min(1, errorMessages.required).max(100, errorMessages.maxLength(100)),
+  lastName: z.string().min(1, errorMessages.required).max(255, errorMessages.maxLength(255)),
+})
+
+export type SignUpFormData = z.infer<typeof signUpValidation>
