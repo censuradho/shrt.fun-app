a8ae132081fcca8113f246fbd4ac6a99338f0d4c
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 13:29:18 -0300

    feat(errors): add errorMessages constant for form validation

---
+++ src/constants/errorMessages.ts
@@ -0,0 +1,6 @@
+
+export const errorMessages = {
+  required: 'Campo obrigatório',
+  maxLength: (max: number) => `O campo deve conter no máximo ${max} caracteres`,
+  minLength: (min: number) => `O campo deve conter no mínimo ${min} caracteres`,
+}
