c576d003e26d9fedbbcd5c70f369ee98c0ee67a8
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 11:29:32 -0300

    feat(auth): enhance sign-in handling with error management and loading state

--- src/screens/public/signin/hook/useSignInViewModel.tsx
+++ src/screens/public/signin/hook/useSignInViewModel.tsx
@@ -1,21 +1,38 @@
 import { useForm } from "react-hook-form"
 import { signInValidation, type SignInFormData } from "../validations"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useAuth } from "@/contexts/auth/auth.context"
+import { isApiError } from "@/lib/supabase"
 
 export function useSignInViewModel () {
+  const { 
+    signIn,
+    isLoading
+  } = useAuth()
+
   const form = useForm<SignInFormData>({
     resolver: zodResolver(signInValidation),
     mode: 'onChange'
   })
 
   const handleSubmit = async (data: SignInFormData) => {
-
+    try {
+      await signIn(data.email, data.password)
+    } catch (error) {
+      if (isApiError(error)) {
+        if (error.status === 400) {
+          form.setError('email', { message: 'E-mail ou senha inválidos' })
+          form.setError('password', { message: 'E-mail ou senha inválidos' })
+        }
+      }
+    }
   }
 
   return {
     form: {
       ...form,
       handleSubmit: form.handleSubmit(handleSubmit)
-    }
+    },
+    isLoading
   }
 }
