047484dd714ebd3fbfc7dc1fd3a45762edf0448a
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 15:34:38 -0300

    feat(routes): add SignUp route to AppRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { paths } from "@/constants/routes";
 import { SignInScreen } from "@/screens/public/signin";
+import { SignUpScreen } from "@/screens/public/signUp";
 import { Route, Routes } from "react-router";
 
 
@@ -7,6 +8,7 @@ export function AppRoutes () {
   return (
     <Routes>
       <Route path={paths.public.signin} element={<SignInScreen />} />
+      <Route path={paths.public.signUp} element={<SignUpScreen />} />
     </Routes>
   )
 }
