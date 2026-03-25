1405510c1ef558ef134d201b8c3fc447e1546842
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 13:05:56 -0300

    feat(navigation): implement IconNavigation component for navigation links

---
+++ src/layout/main/components/IconNavigation.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router"
+import { navigation } from "../constants"
+import { Icon } from "@/components/icons"
+import { cn } from "@/lib/utils"
+
+export function IconNavigation() {
+  const renderNavigation = navigation.map((value, index) => (
+    <li key={index} className="flex">
+      <NavLink 
+        end
+        to={value.path}
+        className={({ isActive }) => cn(
+          'w-full flex gap-2 items-center hover:bg-muted rounded-md hover:text-foreground p-2',
+          'text-sm text-card-foreground',
+          isActive && 'hover:bg-accent bg-accent text-foreground'
+        )}
+      >
+        <Icon name={value.icon} size={20} />
+      </NavLink>
+    </li>
+  ))
+  return (
+    <div className="w-full flex-col flex flex-1 px-2">
+      <button className="bg-primary-500 rounded text-primary-foreground p-2 mb-6 flex items-center justify-center cursor-pointer hover:bg-primary-600">
+        <Icon name="Plus" size={20} />
+      </button>
+      <nav>
+        <ul className="flex flex-col gap justify-center items-center gap-4 my-10">
+          {renderNavigation}
+        </ul>
+      </nav>
+    </div>
+  )
+}
