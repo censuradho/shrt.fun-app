342f1fd6b18e169f2776c4f0b2b0355982ee7746
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 12:16:06 -0300

    feat(account-menu): add AccountMenu component with user details and sign-out functionality

---
+++ src/layout/main/components/AccountMenu.tsx
@@ -0,0 +1,50 @@
+import { Icon } from "@/components/icons"
+import { LetterCircle } from "@/components/LetterCircle"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import type { Me } from "@/services/api/auth/types"
+
+interface AccountMenuProps {
+  me?: Me | null
+  onSignOut?: () => void
+}
+
+export function AccountMenu (props: AccountMenuProps) {
+  const { me, onSignOut } = props
+
+  if (!me) return null
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="hover:bg-popover p-1 rounded">
+        <div className="flex items-center gap-2">
+          <LetterCircle className="bg-blitzit-blue min-w-7 min-h-7" letter={me.firstName} />
+          <strong className="text-sm font-medium">{me.username}</strong>
+          <Icon size={12} name="ChevronDown" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[300px]" align="end">
+        <DropdownMenuItem className="flex items-center gap-2 hover:text-inherit">
+          <Icon name="User" />
+          <div className="flex flex-col">
+            <strong className="text-sm font-medium">{me.firstName} {me.lastName}</strong>
+            <span className="text-sm font-normal text-muted-foreground!">{me.email}</span>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem 
+          onClick={onSignOut}
+          className="gap-2 text-card-foreground cursor-pointer"
+        >
+          <Icon name="LogOut" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
