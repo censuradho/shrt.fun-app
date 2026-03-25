53244d5ed14d7d9033f6ec0f7c93ecc35b0b9b94
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 20:47:36 -0300

    feat(success-create-dialog): enhance layout and functionality with CopyButton and icon integration

--- src/screens/private/link/create/components/SuccessCreateDialog.tsx
+++ src/screens/private/link/create/components/SuccessCreateDialog.tsx
@@ -1,9 +1,12 @@
+import { CopyButton } from "@/components/CopyButton"
+import { Icon } from "@/components/icons"
 import {
   Dialog,
   DialogContent,
   DialogTitle,
   DialogHeader
 } from "@/components/ui/dialog"
+import { copyToClipboard } from "@/utils/copyToClipboard"
 
 interface SuccessCreateDialogProps {
   open: boolean
@@ -19,13 +22,18 @@ export function SuccessCreateDialog ({ open, onOpenChange, link }: SuccessCreate
           <DialogTitle>Seu link está pronto! 🎉</DialogTitle>
           <p>Copie o link abaixo para compartilhar ou escolha uma plataforma para compartilhá-lo.</p>
         </DialogHeader>
-        <div className="bg-popover rounded-md px-2 py-4 flex flex-col items-center justify-center gap-2">
+        <div className="bg-popover rounded-md px-2 py-4 flex items-center justify-between gap-4">
           <a 
             href={link} 
             target="_blank" 
             rel="noopener noreferrer"
-            className="text-link"
-          >{link}</a>
+            className="text-link hover:underline flex items-center gap-1"
+          > <Icon size={14} name="ExternalLink" />{link} </a>
+          <CopyButton 
+            onClick={() => copyToClipboard(link)}
+            className="hover:bg-card"
+          />
+   
         </div>
       </DialogContent>
     </Dialog>
