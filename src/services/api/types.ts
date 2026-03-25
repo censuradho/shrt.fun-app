1f0315bf0886421e389f36c31657ebc06cb9e9f1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 15:15:12 -0300

    feat(api): add pagination interfaces for handling cursor-based pagination

--- src/services/api/types.ts
+++ src/services/api/types.ts
@@ -2,3 +2,13 @@ export interface IApiErrorMessage {
   title?: string
   description: string
 }
+
+export interface PaginationParams {
+  cursor?: string | null
+  limit?: number | null
+}
+
+export interface PaginationResult<T> {
+  data: T[]
+  nextCursor: string | null
+}
