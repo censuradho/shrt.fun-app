0bb9bf9deae3fb30949c1a7534597e954a6c942e
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 15:59:23 -0300

    feat(hooks): implement useCreateLinkViewModel for link creation logic

---
+++ src/screens/private/link/create/hooks/useCreateLinkViewModel.ts
@@ -0,0 +1,44 @@
+import { useCreateUrlMutation } from "@/services/api/url/queries";
+import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
+import { useForm } from "react-hook-form";
+import { createLinkValidation, type CreateLinkFormData } from "../validations";
+import { useState } from "react";
+
+interface SuccessModalData {
+  shortUrl: string;
+}
+
+export function useCreateLinkViewModel () {
+  const [successModal, setSuccessModal] = useState<SuccessModalData | null>(null);
+  
+  const form = useForm<CreateLinkFormData>({
+    resolver: standardSchemaResolver(createLinkValidation),
+    mode: 'onChange'
+  })
+
+  const {
+    mutate,
+    isPending
+  } = useCreateUrlMutation()
+  const handleSubmit = (data: CreateLinkFormData) => {
+    mutate({
+      url: data.url,
+      slug: data.slug || undefined
+    }, {
+      onSuccess: ({ data }) => {
+        form.reset()
+        setSuccessModal(data)
+      }
+    })
+  }
+
+  return {
+    form: {
+      ...form,
+      handleSubmit: form.handleSubmit(handleSubmit)
+    },
+    isPending,
+    successModal,
+    setSuccessModal
+  }
+}
