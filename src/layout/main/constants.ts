ab416d8f82dd9c288b798221b3c1e6ed3445c5a1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 09:23:08 -0300

    feat(navigation): add navigation constants for main layout

---
+++ src/layout/main/constants.ts
@@ -0,0 +1,26 @@
+import type { IconNames } from "@/components/icons";
+import { paths } from "@/constants/routes";
+
+interface NavigationNode {
+  label: string;
+  path: string
+  icon: IconNames
+}
+
+export const navigation: NavigationNode[] = [
+  {
+    label: "Visão geral",
+    path: paths.private.root,
+    icon: "LayoutGrid"
+  },
+  {
+    label: "Meus links",
+    path: paths.private.link.list,
+    icon: "Link"
+  },
+  {
+    label: "Analytics",
+    path: paths.public.signin,
+    icon: "ChartSpline"
+  }
+]
