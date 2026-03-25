3ff94c77b665b76b496a693c93fd436ed8b26f86
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 19:22:48 -0300

    feat(routes): add loading state handling in PrivateRoute component

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,9 +1,12 @@
+import { PageLoader } from "@/components/PageLoader";
 import { paths } from "@/constants/routes";
 import { useAuth } from "@/contexts/auth/auth.context";
 import { Navigate, Outlet } from "react-router";
 
 export function PrivateRoute () {
-  const { supabaseUser } = useAuth()
+  const { supabaseUser, isLoading } = useAuth()
+
+  if (isLoading) return <PageLoader />
 
   if (!supabaseUser) return <Navigate to={paths.public.signin} />
   
