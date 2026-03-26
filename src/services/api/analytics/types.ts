202e0a906d18c3ae1477b0c3597997496f668f52
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-26 13:31:31 -0300

    refactor(api): rename FindHistCountByLocationResponse to HitsCountByLocation for clarity

--- src/services/api/analytics/types.ts
+++ src/services/api/analytics/types.ts
@@ -1,5 +1,6 @@
-export interface FindHistCountByLocationResponse {
+export interface HitsCountByLocation {
   clicks: number
   country?: string | null
   city?: string | null
 }
+export type FindHistCountByLocationResponse = HitsCountByLocation[]
