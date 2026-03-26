d2692fe9b355c47f40828324cda17821e6e70078
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-26 13:35:12 -0300

    refactor(errors): remove RATE_LIMIT_EXCEEDED error message from API_ERROR_MESSAGES

--- src/services/api/errors.ts
+++ src/services/api/errors.ts
@@ -4,7 +4,4 @@ import { URL_ERROR_MESSAGES } from "./url/errors";
 export const API_ERROR_MESSAGES = {
   ...AUTH_ERROR_MESSAGES,
   ...URL_ERROR_MESSAGES,
-  RATE_LIMIT_EXCEEDED: {
-    message: 'Você excedeu o limite de requisições. Por favor, tente novamente mais tarde.'
-  }
 }
