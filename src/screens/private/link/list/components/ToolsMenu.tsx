f776bc7ccd0fe0f49a32b772632a810a87ef80fa
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 23:54:57 -0300

    feat(ToolsMenu): add Select component for active status management and enhance view options

--- src/screens/private/link/list/components/ToolsMenu.tsx
+++ src/screens/private/link/list/components/ToolsMenu.tsx
@@ -2,6 +2,7 @@ import { Icon } from "@/components/icons";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { ViewEnum, type ViewEnumType } from "../hooks/useLinkListViewModel";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 interface ToolsMenuProps {
    onSelectAll: () => void;
@@ -9,10 +10,20 @@ interface ToolsMenuProps {
    onToggleView: (view: ViewEnumType) => void;
    view: ViewEnumType
    selectedCount?: number
+   onChangeIsActive?: (status: 'true' | 'false') => void
+   isActive?: string
 }
 
 export function ToolsMenu (props: ToolsMenuProps) {
-  const { onSelectAll, isSelectedAll, onToggleView, view, selectedCount } = props
+  const { 
+    onSelectAll, 
+    isSelectedAll, 
+    onToggleView, 
+    view, 
+    selectedCount,
+    isActive,
+    onChangeIsActive
+  } = props
 
   return (
     <div className="flex justify-between py-4 sticky top-0 z-10 bg-background/90 backdrop-blur-lg">
@@ -29,36 +40,50 @@ export function ToolsMenu (props: ToolsMenuProps) {
           {selectedCount} selecionado{selectedCount !== 1 && 's'}
         </label>
       </div>
-      <ToggleGroup 
-        size="lg" 
-        type="single" 
-        defaultValue={ViewEnum.LIST}
-        value={view} 
-        onValueChange={onToggleView}
-        className="hidden lg:flex"
-      >
-        <ToggleGroupItem 
-          value={ViewEnum.LIST} 
-          aria-label="Exibir em lista resumida"
-          className="data-[state=on]:bg-accent"
+      <div className="flex items-center gap-4">
+        <ToggleGroup 
+          size="lg" 
+          type="single" 
+          defaultValue={ViewEnum.LIST}
+          value={view} 
+          onValueChange={onToggleView}
+          className="hidden lg:flex"
         >
-          <Icon size={20} name="List" />
-        </ToggleGroupItem>
-        <ToggleGroupItem 
-          value={ViewEnum.RICH_LIST} 
-          aria-label="Exibir em lista completa"
-          className="data-[state=on]:bg-accent"
+          <ToggleGroupItem 
+            value={ViewEnum.LIST} 
+            aria-label="Exibir em lista resumida"
+            className="data-[state=on]:bg-accent"
+          >
+            <Icon size={20} name="List" />
+          </ToggleGroupItem>
+          <ToggleGroupItem 
+            value={ViewEnum.RICH_LIST} 
+            aria-label="Exibir em lista completa"
+            className="data-[state=on]:bg-accent"
+          >
+            <Icon size={20} name="Columns3" className="rotate-90" />
+          </ToggleGroupItem>
+          <ToggleGroupItem 
+            value={ViewEnum.GRID} 
+            aria-label="Exibir em lista completa"
+            className="data-[state=on]:bg-accent"
+          >
+            <Icon size={20} name="LayoutGrid" />
+          </ToggleGroupItem>
+        </ToggleGroup>
+        <Select
+          onValueChange={onChangeIsActive}
+          value={isActive}
         >
-          <Icon size={20} name="Columns3" className="rotate-90" />
-        </ToggleGroupItem>
-        <ToggleGroupItem 
-          value={ViewEnum.GRID} 
-          aria-label="Exibir em lista completa"
-          className="data-[state=on]:bg-accent"
-        >
-          <Icon size={20} name="LayoutGrid" />
-        </ToggleGroupItem>
-      </ToggleGroup>
+          <SelectTrigger className="w-full max-w-48">
+            <SelectValue placeholder="Selecione uma opção" />
+          </SelectTrigger>
+          <SelectContent className="bg-card">
+            <SelectItem value="true">Ativo</SelectItem>
+            <SelectItem value="false">Inativo</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   )
 }
