2cec7a7d5ba2bfc19f04c1fa9e1484bd4ba7df41
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 16:25:43 -0300

    feat(navigation): replace button with Link for create action in IconNavigation

--- src/layout/main/components/IconNavigation.tsx
+++ src/layout/main/components/IconNavigation.tsx
@@ -1,7 +1,8 @@
-import { NavLink } from "react-router"
+import { Link, NavLink } from "react-router"
 import { navigation } from "../constants"
 import { Icon } from "@/components/icons"
 import { cn } from "@/lib/utils"
+import { paths } from "@/constants/routes"
 
 export function IconNavigation() {
   const renderNavigation = navigation.map((value, index) => (
@@ -21,9 +22,9 @@ export function IconNavigation() {
   ))
   return (
     <div className="w-full flex-col flex flex-1 px-2">
-      <button className="bg-primary-500 rounded text-primary-foreground p-2 mb-6 flex items-center justify-center cursor-pointer hover:bg-primary-600">
+      <Link to={paths.private.link.create} className="bg-primary-500 rounded text-primary-foreground p-2 mb-6 flex items-center justify-center cursor-pointer hover:bg-primary-600">
         <Icon name="Plus" size={20} />
-      </button>
+      </Link>
       <nav>
         <ul className="flex flex-col gap justify-center items-center gap-4 my-10">
           {renderNavigation}
