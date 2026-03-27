76e04d1da1354027b8051358647608484e6aea09
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 16:00:17 -0300

    refactor(analytics): replace findLocationHits with findHitsByCity and findHitsByCountry functions

--- src/services/api/analytics/index.ts
+++ src/services/api/analytics/index.ts
@@ -1,21 +1,32 @@
 import { api } from "..";
-import type { FindLocationHitsPaginationParamsDto } from "../dtos";
-import type { FindLocationHitsResponse, FindHistCountByLocationResponse } from "./types";
+import type { OffsetPaginationParams } from "../types";
+import type { FindHistCountByLocationResponse, FindHitsByCityResponse, FindHitsByCountryResponse } from "./types";
 
 async function findHistCountByLocation (id: string) {
   const { data } = await api.get<FindHistCountByLocationResponse>(`/analytics/locations/hits/url/${id}`);
   return data
 }
 
-async function findLocationHits (params: FindLocationHitsPaginationParamsDto) {
-  const { data } = await api.get<FindLocationHitsResponse>('/analytics/locations/hits/url', {
+
+async function findHitsByCity (params?: OffsetPaginationParams) {
+  const { data } = await api.get<FindHitsByCityResponse>('/analytics/locations/hits/cities', {
     params
   })
 
   return data
 }
 
+async function findHitsByCountry (params?: OffsetPaginationParams) {
+  const { data } = await api.get<FindHitsByCountryResponse>('/analytics/locations/hits/countries', {
+    params
+  })
+
+  return data
+}
+
+
 export const analyticsService = {
   findHistCountByLocation,
-  findLocationHits
+  findHitsByCity,
+  findHitsByCountry
 }
