b9d4dde96225c0b213012799ab553af94d812959
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 16:31:34 -0300

    feat(App): wrap application in ThemeProvider for theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 import { AuthProvider } from "./contexts/auth/auth.context";
+import { ThemeProvider } from "./contexts/theme.context.tsx";
 import { Toaster } from "sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
@@ -9,17 +10,19 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <Toaster 
-            theme="dark" 
-            richColors 
-            position="bottom-right"
-            closeButton
-          />
-          <AppRoutes />
-        </AuthProvider>
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <Toaster
+              theme="dark"
+              richColors
+              position="bottom-right"
+              closeButton
+            />
+            <AppRoutes />
+          </AuthProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
