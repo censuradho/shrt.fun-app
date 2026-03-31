f7e81f43e84a43b3d442ef64e7a5cab81f55b72b
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-30 21:15:30 -0300

    feat(AccountMenu): implement settings option

--- src/layout/main/components/AccountMenu.tsx
+++ src/layout/main/components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/Button"
 import { Icon } from "@/components/icons"
 import { LetterCircle } from "@/components/LetterCircle"
 import {
@@ -8,8 +9,10 @@ import {
   DropdownMenuTrigger,
   DropdownMenuLabel
 } from "@/components/ui/dropdown-menu"
+import { PLANS_LABELS } from "@/constants/plans"
 import { useTheme } from "@/contexts/theme.context"
 import type { Me } from "@/services/api/auth/types"
+import { toastyComingSoon } from "@/utils/toastyComingSoon"
 
 interface AccountMenuProps {
   me?: Me | null
@@ -47,6 +50,18 @@ export function AccountMenu (props: AccountMenuProps) {
           </div>
         </div>
         <DropdownMenuSeparator />
+        <div className="flex items-center gap-2 hover:text-inherit hover:bg-transparent py-1 px-2 justify-between" >
+          <div className="flex flex-col">
+            <span className="text-sm">{PLANS_LABELS[me.plan.name as keyof typeof PLANS_LABELS]}</span>
+          </div>
+          <Button 
+            size="sm"
+            onClick={toastyComingSoon}
+          >
+            Upgrade
+          </Button>
+        </div>
+        <DropdownMenuSeparator />
         <DropdownMenuLabel className="uppercase text-xxs font-semibold text-card-foreground">Tema</DropdownMenuLabel>
         <DropdownMenuItem 
           className="gap-4 text-card-foreground cursor-pointer" onClick={() => setTheme("dark")}>
@@ -64,7 +79,14 @@ export function AccountMenu (props: AccountMenuProps) {
         <DropdownMenuSeparator />
         <DropdownMenuItem 
           onClick={onSignOut}
-          className="gap-2 text-card-foreground cursor-pointer"
+          className="gap-2 text-card-foreground cursor-pointer py-2"
+        >
+          <Icon name="Settings" />
+          <span>Configurações</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem 
+          onClick={onSignOut}
+          className="gap-2 text-card-foreground cursor-pointer py-2"
         >
           <Icon name="LogOut" />
           <span>Sair</span>
