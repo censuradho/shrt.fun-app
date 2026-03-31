4b5121bb52fc379eb65a80b9904d13d6a2004f3d
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-30 21:03:52 -0300

    feat(URL_ERROR_MESSAGES): add MONTHLY_LINK_LIMIT_REACHED & DAILY_LINK_LIMIT_REACHED errors

--- src/services/api/url/errors.ts
+++ src/services/api/url/errors.ts
@@ -3,5 +3,11 @@ import type { IApiErrorMessage } from "../types";
 export const URL_ERROR_MESSAGES: Record<string, IApiErrorMessage> = {
   SHORT_URL_ALREADY_EXISTS: {
     description: 'O slug personalizado que você escolheu já está em uso. Por favor, escolha outro slug ou deixe em branco para gerar um automaticamente.'
+  },
+  MONTHLY_LINK_LIMIT_REACHED: {
+    description: 'Você atingiu o limite mensal de criação de links. Por favor, aguarde até o próximo mês para criar mais links ou considere atualizar para um plano premium para aumentar seu limite.'
+  },
+  DAILY_LINK_LIMIT_REACHED: {
+    description: 'Você atingiu o limite diário de criação de links. Por favor, aguarde até amanhã para criar mais links ou considere atualizar para um plano premium para aumentar seu limite.'
   }
 }
