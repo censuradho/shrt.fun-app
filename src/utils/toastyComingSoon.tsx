c02340655810f88cafd3a4ceab6eec9deb3ed734
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-30 21:15:10 -0300

    feat(toastyComingSoon): implement TODO tosat

---
+++ src/utils/toastyComingSoon.tsx
@@ -0,0 +1,7 @@
+import { toast } from "sonner";
+
+export function toastyComingSoon () {
+  toast.message(
+    <span>TODO: Essa funcionalidade ainda não foi implementada <span className="text-lg">😭</span></span>
+  )
+}
