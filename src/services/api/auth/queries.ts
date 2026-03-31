4bc59d55a7a7ee83581ac967cb8442add495924f
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 14:56:28 -0300

    feat(auth): add useResetPasswordMutation for email reset functionality

--- src/services/api/auth/queries.ts
+++ src/services/api/auth/queries.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { authService } from "./auth.service";
+import { authGateway } from "@/lib/supabase";
 
 export const useSignUpMutation = () => {
   return useMutation({
@@ -13,5 +14,17 @@ export const useMeQuery = (enabled: boolean) => {
     queryKey: ["auth", "me"],
     queryFn: authService.me,
     enabled,
+  })
+}
+
+export const useResetPasswordMutation = () => {
+  return useMutation({
+    mutationKey: ["auth", "reset-password"],
+    mutationFn: ({
+      email, 
+      options
+    }: {
+      email: string, options?: { redirectTo?: string }
+    }) => authGateway.resetPasswordForEmail(email, options),
   })
 }
