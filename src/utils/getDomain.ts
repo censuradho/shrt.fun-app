55e4c6e0e9cd1d0fb3dc5fa7c3064ae633c134b5
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 20:40:19 -0300

    feat(getDomain): add clearHttp function to remove protocol and www

--- src/utils/getDomain.ts
+++ src/utils/getDomain.ts
@@ -1,6 +1,9 @@
 export const getDomain = (url: string) =>
   url.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')[0]
 
+export const clearHttp = (url: string) =>
+  url.replace(/^(https?:\/\/)?(www\.)?/, '')
+
 export function getLinkFavicon (url: string, size: number = 32) {
   return `https://www.google.com/s2/favicons?domain=${getDomain(url)}&sz=${size}`
 }
