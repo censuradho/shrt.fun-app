87aac50acaf3eff3da80d78dd900aed09fc8eec1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 23:24:59 -0300

    feat(MobileNavigation): add Logo component and update navigation layout

--- src/layout/main/components/MobileNavigation.tsx
+++ src/layout/main/components/MobileNavigation.tsx
@@ -7,8 +7,9 @@ import {
 import { paths } from "@/constants/routes"
 import { cn } from "@/lib/utils"
 import type { PropsWithChildren } from "react"
-import { NavLink } from "react-router"
+import { Link, NavLink } from "react-router"
 import { navigation } from "../constants"
+import Logo from "@/assets/logo-short.svg?react"
 
 interface MobileNavigationProps {
   open: boolean
@@ -47,6 +48,11 @@ export function MobileNavigation (props: PropsWithChildren<MobileNavigationProps
       direction="left"
     >
       <DrawerContent className="w-full bg-card">
+        <div className="w-full flex justify-start px-6 items-center h-14 shrink-0 border-b border-outline">
+          <Link to={paths.private.link.list}>
+            <Logo className="text-foreground w-8 h-8"/>
+          </Link>
+        </div>
         <nav className="w-full flex-1 px-4 my-10">
           <ul 
             className="flex flex-col"
