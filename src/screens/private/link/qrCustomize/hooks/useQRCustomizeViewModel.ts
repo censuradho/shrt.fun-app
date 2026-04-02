dad8868856c297e9d5960952c976ba33d62b895e
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 18:05:09 -0300

    feat(useQRCustomizeViewModel): enhance form initialization with URL data for QR code options

--- src/screens/private/link/qrCustomize/hooks/useQRCustomizeViewModel.ts
+++ src/screens/private/link/qrCustomize/hooks/useQRCustomizeViewModel.ts
@@ -1,16 +1,33 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { qrCodeCustomizeValidations } from "../validations";
+import { qrCodeCustomizeValidations, type QRCodeCustomizeFormData } from "../validations";
+import { useParams } from "react-router";
+import { useFindUrlByIdQuery } from "@/services/api/url/queries";
 
 export function useQRCustomizeViewModel () {
+  const id = useParams().id as string
+
+  const {
+    data: url
+  } = useFindUrlByIdQuery(id)
+
+
   const form = useForm({
     resolver: zodResolver(qrCodeCustomizeValidations),
     defaultValues: {
       watermarkLogo: true
     },
+    values: {
+      watermarkLogo: !!url?.qrCodeOptions?.watermarkLogo,
+      dotsColor: url?.qrCodeOptions?.dotsColor,
+      backgroundColor: url?.qrCodeOptions?.backgroundColor,
+      centerLogo: url?.qrCodeOptions?.centerLogo,
+      cornersDotStyle: url?.qrCodeOptions?.cornersDotStyle as QRCodeCustomizeFormData['cornersDotStyle'],
+      cornersSquareStyle: url?.qrCodeOptions?.cornersSquareStyle as QRCodeCustomizeFormData['cornersSquareStyle'],
+      dotsStyle: url?.qrCodeOptions?.dotsStyle as QRCodeCustomizeFormData['dotsStyle'],
+    },
     mode: 'onChange'
   })
-
 
   return {
     form,
