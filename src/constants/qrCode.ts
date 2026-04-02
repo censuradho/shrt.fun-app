50d99c7079e44242dacc73d7d6e9d98e23608bee
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 19:26:36 -0300

    feat(qrCode): add QR_CODE_FREE_COLORS constant for customizable QR code colors

--- src/constants/qrCode.ts
+++ src/constants/qrCode.ts
@@ -27,3 +27,14 @@ export const QRCodeCornersDotStyleEnum = {
 export type QRCodeCornersSquareStyle = typeof QRCodeCornersSquareStyleEnum[keyof typeof QRCodeCornersSquareStyleEnum]
 
 export type QRCodeCornersDotStyle = typeof QRCodeCornersDotStyleEnum[keyof typeof QRCodeCornersDotStyleEnum]
+
+export const QR_CODE_FREE_COLORS = [
+  "#D84280",
+  "#DE3121",
+  "#EF8000",
+  "#198639",
+  "#229CE0",
+  "#2A5BD7",
+  "#6B52D1",
+  "#000000",
+]
