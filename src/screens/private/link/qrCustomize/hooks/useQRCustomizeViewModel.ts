4365b43793ab4db353c87821c998c7858ecfd833
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 15:52:45 -0300

    feat(hooks): add useQRCustomizeViewModel for QR code customization form handling

---
+++ src/screens/private/link/qrCustomize/hooks/useQRCustomizeViewModel.ts
@@ -0,0 +1,16 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { qrCodeCustomizeValidations } from "../validations";
+
+export function useQRCustomizeViewModel () {
+  const form = useForm({
+    resolver: zodResolver(qrCodeCustomizeValidations),
+    defaultValues: {
+      watermarkLogo: true
+    }
+  })
+
+  return {
+    form
+  }
+}
