a1926eea9a3723e59f2d1ecd13f34a530eaa7c69
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 19:26:50 -0300

    feat(colorsCard): enhance color customization with tooltip for free users

--- src/screens/private/link/qrCustomize/components/ColorsCard.tsx
+++ src/screens/private/link/qrCustomize/components/ColorsCard.tsx
@@ -1,14 +1,36 @@
-import { ColorPickerHook } from '../../create/components/ColorPickerHook'
 import { HexColorInputHook } from '@/components/hook/HexColorInputHook'
+import { Icon } from '@/components/icons'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { PLANS_LABELS } from '@/constants/plans'
+import { useAuth } from '@/contexts/auth/auth.context'
 import { useFormState, type UseFormReturn } from 'react-hook-form'
+import { ColorPickerHook } from '../../create/components/ColorPickerHook'
 import type { QRCodeCustomizeFormData } from '../validations'
-
 interface ColorsCardProps {
   form: UseFormReturn<QRCodeCustomizeFormData>
 }
 
 export function ColorsCard ({ form }: ColorsCardProps) {
+  const { isFree } = useAuth()
+
   const { errors } = useFormState({ control: form.control })
+
+
+  const renderTooltipLockInfo = () => {
+    if (!isFree) return null
+
+    return (
+      <Tooltip>
+        <TooltipTrigger><Icon name="Lock" size={12} /></TooltipTrigger>
+        <TooltipContent>
+          <p className="text-xs">
+            Personalizar as cores do QR Code é um recurso disponível apenas para usuários {PLANS_LABELS.GROWTH}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    )
+  }
+  
   return (
     <div>
       <h2 className="text-lg">Escolha suas cores</h2>
@@ -22,17 +44,22 @@ export function ColorsCard ({ form }: ColorsCardProps) {
         </div>
       </div>
       <div className="mt-4 grid grid-cols-2 gap-3">
+
         <HexColorInputHook
           control={form.control}
           name="dotsColor"
           label="Code"
           errorMessage={errors.dotsColor?.message}
+          disabled={isFree}
+          tailLabel={renderTooltipLockInfo()}
         />
         <HexColorInputHook
           control={form.control}
           name="backgroundColor"
           label="Background"
+          disabled={isFree}
           errorMessage={errors.backgroundColor?.message}
+          tailLabel={renderTooltipLockInfo()}
         />
       </div>
     </div>
