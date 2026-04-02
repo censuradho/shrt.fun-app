c3124f627ac8fe0ec78a331ff6793379fec53813
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 15:53:08 -0300

    feat(ColorPicker): add ColorPickerHook component for color selection in forms

---
+++ src/screens/private/link/create/components/ColorPickerHook.tsx
@@ -0,0 +1,46 @@
+import { ColorToggle } from "@/components/ColorToggle"
+import { RadioGroup as RadioGroupPrimitive } from "radix-ui"
+import { Controller, type Control, type FieldValues, type Path } from "react-hook-form"
+
+interface ColorPickerHookProps<T extends FieldValues> {
+  control: Control<T>
+  name: Path<T>
+}
+
+const colors = [
+  "#D84280",
+  "#DE3121",
+  "#EF8000",
+  "#198639",
+  "#229CE0",
+  "#2A5BD7",
+  "#6B52D1",
+  "#000000",
+]
+
+export function ColorPickerHook<T extends FieldValues>(props: ColorPickerHookProps<T>) {
+  const { control, name } = props
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <RadioGroupPrimitive.Root
+          className="flex items-center flex-wrap gap-2"
+          value={field.value ?? ""}
+          onValueChange={(value) => value && field.onChange(value)}
+        >
+          {colors.map(color => (
+            <ColorToggle
+              key={color}
+              label={color}
+              value={color}
+              selected={field.value === color}
+            />
+          ))}
+        </RadioGroupPrimitive.Root>
+      )}
+    />
+  )
+}
