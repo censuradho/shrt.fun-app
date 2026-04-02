f067f2bcee2b8e9992cfb6cd05d608393f4a1caf
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 18:14:50 -0300

    feat(plans): define PLANS_ENUM and corresponding type for plan options

--- src/constants/plans.ts
+++ src/constants/plans.ts
@@ -1,3 +1,10 @@
+export const PLANS_ENUM = {
+  GROWTH: 'GROWTH',
+  FREE: 'FREE',
+} as const
+
+export type PLANS = typeof PLANS_ENUM[keyof typeof PLANS_ENUM]
+
 export const PLANS_LABELS = {
   GROWTH: 'Plano Growth (Em breve)',
   FREE: 'Plano Free (Grátis)',
