000d3152948e3164155f80520d205fd394ab9c16
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 16:41:51 -0300

    fix(QrCodeCustomizeScreen): conditionally render QRCodePreviewDialog and QRCodePreviewCard based on screen size

--- src/screens/private/link/qrCustomize/index.tsx
+++ src/screens/private/link/qrCustomize/index.tsx
@@ -6,26 +6,30 @@ import { useQRCustomizeViewModel } from "./hooks/useQRCustomizeViewModel";
 import { cn } from "@/lib/utils";
 import { QRCodePreviewCard } from "./components/QRCodePreview";
 import { QRCodePreviewDialog } from "./components/QRCodePreviewDialog";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
 export function QrCodeCustomizeScreen () {
   const { form } = useQRCustomizeViewModel()
+  const { isMobile } = useWindowSize()
 
   return (
     <div className="grid md:grid-cols-[720px_1fr] flex-1 h-full">
       <section className="flex flex-col bg-card px-4 mx-1 mt-1 rounded-tl-2xl relative pb-10">
         <div className="w-full max-w-[600px] flex-1 mx-auto mt-10 ">
-          <QRCodePreviewDialog
-            form={form}
-            trigger={(
-              <Button
-                variant="ghost"
-                className="w-full mb-8 md:hidden sticky top-1 z-10 shadow-2xl"
-                headIcon={{ name: 'QrCode' }}
-              >
+          {isMobile && (
+            <QRCodePreviewDialog
+              form={form}
+              trigger={(
+                <Button
+                  variant="ghost"
+                  className="w-full mb-8 md:hidden sticky top-1 z-10 shadow-2xl"
+                  headIcon={{ name: 'QrCode' }}
+                >
                 Preview do seu QR Code
-              </Button>
-            )}
-          />
+                </Button>
+              )}
+            />
+          )}
           <header className="flex justify-between items-center gap-4">
             <h1 className="text-2xl font-bold">Personalize o design</h1>
           </header>
@@ -49,11 +53,13 @@ export function QrCodeCustomizeScreen () {
           <Button>Salvar</Button>
         </div>
       </section>
-      <div className="hidden md:flex items-center justify-center flex-1">
-        <QRCodePreviewCard 
-          form={form}
-        />
-      </div>
+      {!isMobile && (
+        <div className="hidden md:flex items-center justify-center flex-1">
+          <QRCodePreviewCard 
+            form={form}
+          />
+        </div>
+      )}
     </div>
   )
 }
