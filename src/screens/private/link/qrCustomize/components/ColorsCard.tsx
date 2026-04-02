95cfbf3d5025d49ce317ca5caa585da7e6f36e8f
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 15:51:49 -0300

    feat(ColorsCard): add ColorsCard component for color selection functionality

---
+++ src/screens/private/link/qrCustomize/components/ColorsCard.tsx
@@ -0,0 +1,37 @@
+import { ColorPickerHook } from '../../create/components/ColorPickerHook'
+import { HexColorInputHook } from '@/components/hook/HexColorInputHook'
+import { type UseFormReturn } from 'react-hook-form'
+import type { QRCodeCustomizeFormData } from '../validations'
+
+interface ColorsCardProps {
+  form: UseFormReturn<QRCodeCustomizeFormData>
+}
+
+export function ColorsCard ({ form }: ColorsCardProps) {
+  return (
+    <div>
+      <h2 className="text-lg">Escolha suas cores</h2>
+      <div className="mt-4">
+        <h3 className="text-xxs text-card-foreground uppercase">Preset</h3>
+        <div className='mt-2'>
+          <ColorPickerHook
+            control={form.control}
+            name="dotsColor"
+          />
+        </div>
+      </div>
+      <div className="mt-4 grid grid-cols-2 gap-3">
+        <HexColorInputHook
+          control={form.control}
+          name="dotsColor"
+          label="Code"
+        />
+        <HexColorInputHook
+          control={form.control}
+          name="backgroundColor"
+          label="Background"
+        />
+      </div>
+    </div>
+  )
+}
