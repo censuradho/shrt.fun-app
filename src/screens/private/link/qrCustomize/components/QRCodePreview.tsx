3f776579a6855965d2b494b04c5495bd02f3a526
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 16:34:04 -0300

    feat(QRCodePreviewCard): add component for QR code preview with form integration

---
+++ src/screens/private/link/qrCustomize/components/QRCodePreview.tsx
@@ -0,0 +1,45 @@
+import { useWatch, type UseFormReturn } from "react-hook-form";
+import type { QRCodeCustomizeFormData } from "../validations";
+import { useGenerateQrCodePreviewMutation } from "@/services/api/url/queries";
+import { useEffect } from "react";
+import { QRCodePreview } from "../../create/components/QRCodePreview";
+
+interface QRCodePreviewCardProps {
+  form: UseFormReturn<QRCodeCustomizeFormData>
+}
+
+
+export function QRCodePreviewCard ({ form }: QRCodePreviewCardProps) {
+  const specs = useWatch({
+    control: form.control,
+  })
+
+  const {
+    data,
+    isPending,
+    mutate
+  } = useGenerateQrCodePreviewMutation()
+
+  useEffect(() => {
+    mutate({
+      ...(specs.dotsColor && ({
+        dotsColor: specs.dotsColor,
+      })),
+      backgroundColor: specs.backgroundColor,
+      dotsStyle: specs.dotsStyle,
+      cornersDotStyle: specs.cornersDotStyle,
+      cornersSquareStyle: specs.cornersSquareStyle,
+      centerLogo: specs.centerLogo,
+    })
+  }, [specs])
+
+  return (
+    <>
+      <QRCodePreview 
+        src={data?.qrCode || null} 
+        isPending={isPending}
+        size={18}
+      />
+    </>
+  )
+}
