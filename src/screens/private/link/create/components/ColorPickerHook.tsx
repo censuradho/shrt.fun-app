95722bc005178c2810fa74e7edfe45f03a3c8aaf
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 19:27:42 -0300

    feat(ColorPickerHook): replace hardcoded colors with QR_CODE_FREE_COLORS constant

--- src/screens/private/link/create/components/ColorPickerHook.tsx
+++ src/screens/private/link/create/components/ColorPickerHook.tsx
@@ -1,4 +1,5 @@
 import { ColorToggle } from "@/components/ColorToggle"
+import { QR_CODE_FREE_COLORS } from "@/constants/qrCode"
 import { RadioGroup as RadioGroupPrimitive } from "radix-ui"
 import { Controller, type Control, type FieldValues, type Path } from "react-hook-form"
 
@@ -6,17 +7,6 @@ interface ColorPickerHookProps<T extends FieldValues> {
   control: Control<T>
   name: Path<T>
 }
-
-const colors = [
-  "#D84280",
-  "#DE3121",
-  "#EF8000",
-  "#198639",
-  "#229CE0",
-  "#2A5BD7",
-  "#6B52D1",
-  "#000000",
-]
 
 export function ColorPickerHook<T extends FieldValues>(props: ColorPickerHookProps<T>) {
   const { control, name } = props
@@ -31,7 +21,7 @@ export function ColorPickerHook<T extends FieldValues>(props: ColorPickerHookPro
           value={field.value ?? ""}
           onValueChange={(value) => value && field.onChange(value)}
         >
-          {colors.map(color => (
+          {QR_CODE_FREE_COLORS.map(color => (
             <ColorToggle
               key={color}
               label={color}
