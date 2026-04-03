d67a5776c193bc9f3d9ee49f1ad5a7b8ad01f743
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-02 22:09:11 -0300

    fix(spinner): add color prop to Spinner component and update styles for dynamic color

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,12 +1,20 @@
-
+import React from 'react'
 import styles from './styles.module.css'
 
 interface SpinnerProps {
   size?: number
+  color?: string
 }
 
-export function Spinner ({ size = 14 }: SpinnerProps) {
+export function Spinner ({ size = 14, color }: SpinnerProps) {
   return (
-    <div className={styles.spinner} style={{ width: size, height: size }}></div>
+    <div
+      className={styles.spinner}
+      style={{
+        width: size,
+        height: size,
+        ...(color && { '--spinner-color': color } as React.CSSProperties),
+      }}
+    />
   )
 }
